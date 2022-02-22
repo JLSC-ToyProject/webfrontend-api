c589ad64da12fdd761d06106095caf6a28067845
HannaChoi <hanna10299@gmail.com>
2022-02-22 23:21:18 +0900

    axios 라이브러리 추가 및 환경별 설정값 단순 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-
 const buildEnv = import.meta.env.VITE_BUILD_ENV
 const apiUrl = import.meta.env.VITE_API_URL
-
 </script>
 
 <template>
-  <h1>빌드 환경 {{ buildEnv }}</h1>
-  <h1>API URL {{ apiUrl }}</h1>
+  <div>
+    <h1>빌드 환경</h1>
+    <h3>{{ buildEnv }}</h3>
+  </div>
+  <div>
+    <h1>API URL</h1>
+    <h3>{{ apiUrl }}</h3>
+  </div>
 </template>
 
 <style>
