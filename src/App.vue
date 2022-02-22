8915bb062da293910e296502b91ccd3e25dd9566
HannaChoi <hanna10299@gmail.com>
2022-02-22 23:16:05 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+
+const buildEnv = import.meta.env.VITE_BUILD_ENV
+const apiUrl = import.meta.env.VITE_API_URL
+
+</script>
+
+<template>
+  <h1>빌드 환경 {{ buildEnv }}</h1>
+  <h1>API URL {{ apiUrl }}</h1>
+</template>
+
+<style>
+</style>
